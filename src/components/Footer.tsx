612a096e6357edc271f7a5f9ef0502a9ac7f17c4
kawasawa <16559662+kawasawa@users.noreply.github.com>
2022-12-03 17:17:20 +0900

    出典を記載

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,6 +6,16 @@ import { constants } from '../constants';
 export const Footer = () => {
   return (
     <AppBar sx={{ position: 'static', display: 'block', padding: 2 }}>
+      <Typography variant="body2" sx={{ color: colors.grey[500], textAlign: 'center' }} gutterBottom>
+        {'Source: '}
+        <Link href={constants.url.openWeatherMap} target="_blank" color="inherit" underline="always">
+          OpenWeatherMap
+        </Link>
+        {', '}
+        <Link href={constants.url.flaticon} target="_blank" color="inherit" underline="always">
+          Flaticon
+        </Link>
+      </Typography>
       <Typography variant="body2" sx={{ color: colors.grey[500], textAlign: 'center' }}>
         {'© '}
         <Link href={constants.url.homepage} target="_blank" color="inherit" underline="always">
